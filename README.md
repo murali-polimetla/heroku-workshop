112afef369652c47bc4e7f5877c4ed8b4eb3cb1d
Jeff <jeff@zilkey.com>
2017-06-20 20:48:06 +0000

    added cloud9 setup script

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Setup on Cloud9
+
 - Create a Cloud9 Account
 - On Cloud9 Connect Github
 - Fork the Github repo
@@ -5,27 +7,10 @@
 - Run these commands
 
   ```
-  sudo service postgresql start
-  createdb heroku-tasks
-  createdb heroku-tasks-test
-
-  nvm install 7
-  nvm alias default 7
-
-  psql -c "ALTER USER ubuntu PASSWORD 'password2';"
-  export DATABASE_URL="postgres://ubuntu:password@localhost/heroku-tasks"
-  export TEST_DATABASE_URL="postgres://ubuntu:password@localhost/heroku-tasks-test"
-
-  sudo -u <user_name> psql -c "ALTER USER <user_name> PASSWORD '<new_password>';"
-
-  npm install -g yarn
-  yarn
-  yarn run migrate
-  yarn run seed
-  yarn run migrate -- --env=test
-  yarn start
+  sh script/setup.sh && . ~/.profile
   ```
 
+## Helpful Links
 
 - https://devcenter.heroku.com/articles/getting-started-with-nodejs#define-a-procfile
 - https://devcenter.heroku.com/articles/release-phase
--- script/setup.sh
+++ script/setup.sh
@@ -1,4 +1,42 @@
+echo "\n-----------------------------"
+echo "Updating Node..."
+echo "-----------------------------\n"
+
+. ~/.profile
+nvm install 7
+nvm alias default 7
+
+echo "\n-----------------------------"
+echo "Setting up PostgreSQL..."
+echo "-----------------------------\n"
 sudo service postgresql start
+createdb heroku-tasks
+createdb heroku-tasks-test
 psql -c "ALTER USER ubuntu PASSWORD 'password';"
+
 echo "export DATABASE_URL=postgres://ubuntu:password@localhost/heroku-tasks" >> ~/.profile
 echo "export TEST_DATABASE_URL=postgres://ubuntu:password@localhost/heroku-tasks-test" >> ~/.profile
+
+. ~/.profile
+
+echo "\n-----------------------------"
+echo "Setting up Yarn..."
+echo "-----------------------------\n"
+
+npm install -g yarn
+yarn
+
+echo "\n-----------------------------"
+echo "Running migrations..."
+echo "-----------------------------\n"
+
+yarn run migrate
+yarn run seed
+yarn run migrate -- --env=test
+
+
+echo "\n-----------------------------"
+echo "Running tests..."
+echo "-----------------------------\n"
+
+yarn test
